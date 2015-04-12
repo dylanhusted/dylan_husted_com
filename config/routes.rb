867456e8508dc69e240183394b9fab969198c07b
Dylan Husted <dhusted1@babson.edu>
2015-04-12 12:16:25 -0400

    Finish about page, contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 DylanHustedCom::Application.routes.draw do
   root 'home#home'
   resources :startups, only: [:show, :index]
-
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
